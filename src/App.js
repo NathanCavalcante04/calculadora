a01232e26d042e804800b3a3d48aac85bb725353
nathan <nathan.cavalcante1010@gmail.com>
2022-03-03 20:34:42 -0300

    calculadora

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class calculadora extends React.Component{
+
+  state = {
+    result:0,
+    num1: null,
+    nu2:null
+  }
+   handleChange = (e) => {
+     this.setState({
+       num1: e.target.value
+     })
+   }
+   handleChange2 = (e) => {
+    this.setState({
+      num2: e.target.value
+    })
+  }
+
+  minus = () => {
+    let{num1, num2} = this.state
+    this.setState({
+      result: num1 - num2
+    })
+  }
+  add = () => {
+    let{num1, num2} = this.state
+    this.setState({
+      result: Number(num1) + Number(num2)
+    })
+  }
+  divisao = () => {
+    let{num1, num2} = this.state
+    this.setState({
+      result: num1 / num2
+    })
+  }
+  multipla = () => {
+    let{num1, num2} = this.state
+    this.setState({
+      result: num1 * num2
+    })
+  }
+  
+
+  
+
+render(){
+  return(
+    <div class="container">
+      <h1>calculadora</h1>
+      <h2> {this.state.result} </h2>
+      <input onChange={this.handleChange}type="number"/>
+      <input onChange={this.handleChange2}type="number"/>
+      <button onClick={this.minus}>-</button>
+      <button onClick={this.add}>+</button>
+      <button onClick={this.divisao}>/</button>
+      <button onClick={this.multipla}>+</button>
+      
     </div>
-  );
+  )
 }
-
-export default App;
+}
